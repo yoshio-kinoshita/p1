72613badc1f0690123ae32b2adb838de052f1f20
yoshio kinoshita <g-ykinoshita@gmo-pg.com>
2013-02-15 18:50:13 +0900

    Signed-off-by: yoshio kinoshita <g-ykinoshita@gmo-pg.com>

--- p1/src/main/java/Main.java
+++ p1/src/main/java/Main.java
@@ -25,12 +25,12 @@ public class Main {
 
 		LogParser logParser = new LogParserImpl();
 
-		Map<String, Result> map = logParser.parser(logfiles,
+		Map<String, Result> resultMap = logParser.parser(logfiles,
 				new ArrayList<String>());
 
 		Map<String, Integer> countMap = new HashMap<>();
 
-		for (Entry<String, Result> entries : map.entrySet()) {
+		for (Entry<String, Result> entries : resultMap.entrySet()) {
 			Result result = entries.getValue();
 			String ip = result.getIp();
 			Integer count = countMap.get(ip);
@@ -42,9 +42,25 @@ public class Main {
 			}
 			countMap.put(ip, count);
 		}
-		List<Entry<String, Result>> entries = new ArrayList<>(map.entrySet());
 
-		Collections.sort(entries, new Comparator<Object>() {
+		List<Entry<String, Integer>> countEntries = new ArrayList<>(
+				countMap.entrySet());
+		Collections.sort(countEntries, new Comparator<Object>() {
+			public int compare(Object obj1, Object obj2) {
+				Map.Entry<String, Integer> ent1 = (Map.Entry<String, Integer>) obj1;
+				Map.Entry<String, Integer> ent2 = (Map.Entry<String, Integer>) obj2;
+				Integer val1 = (Integer) ent1.getValue();
+				Integer val2 = (Integer) ent2.getValue();
+
+				int cnt = val1.compareTo(val2);
+				return -1 * cnt;
+			}
+		});
+
+		List<Entry<String, Result>> resultEntries = new ArrayList<>(
+				resultMap.entrySet());
+
+		Collections.sort(resultEntries, new Comparator<Object>() {
 			public int compare(Object obj1, Object obj2) {
 				Map.Entry<String, Result> ent1 = (Map.Entry<String, Result>) obj1;
 				Map.Entry<String, Result> ent2 = (Map.Entry<String, Result>) obj2;
@@ -62,7 +78,15 @@ public class Main {
 			}
 		});
 
-		for (Entry<String, Result> entry : entries) {
+		for (Entry<String, Integer> entry : countEntries) {
+			System.out.println("count:" + entry.getValue() + " ip:"
+					+ entry.getKey());
+		}
+		
+		System.out.println("---------------");
+		
+
+		for (Entry<String, Result> entry : resultEntries) {
 			Result result = entry.getValue();
 			System.out.println("count:" + countMap.get(result.getIp()) + " ip:"
 					+ result.getIp() + " url:" + result.getUrl() + " access:"
